5ab4d9058656bc26c2eb570c8c04c10bd0996ff5
Ykz-137 <yoskar327alvarado@gmail.com>
2026-07-15 19:15:45 -0500

    feat(model): add exceptions package

---
+++ src/main/java/com/example/batallanaval/model/Exceptions/InvalidShotException.java
@@ -0,0 +1,7 @@
+package com.example.batallanaval.model.Exceptions;
+
+public class InvalidShotException extends RuntimeException {
+    public InvalidShotException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/example/batallanaval/model/Exceptions/OutOfBoardException.java
@@ -0,0 +1,7 @@
+package com.example.batallanaval.model.Exceptions;
+
+public class OutOfBoardException extends RuntimeException {
+    public OutOfBoardException(String message) {
+        super(message);
+    }
+}
